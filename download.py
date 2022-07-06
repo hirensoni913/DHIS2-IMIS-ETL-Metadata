7bfbda093f10a31016c699c6610a2d5ebfdbf442
David Huser <dhuser@baosystems.com>
2022-07-06 21:24:31 +0200

    add dashboard export

--- download.py
+++ download.py
@@ -21,11 +21,12 @@ default_remove_keys = [
 
 def save_json(data, filename, remove_keys=default_remove_keys):
     """Save JSON file to a folder"""
-    data = clean_obj(data, remove_keys)
-    file_path = path.join('metadata', f"{filename}.json")
-    with open(file_path, 'w') as f:
-        json.dump(data, f, indent=2)
-    logger.info(f"Saved file: {file_path}")
+    if data:
+        data = clean_obj(data, remove_keys)
+        file_path = path.join('metadata', f"{filename}.json")
+        with open(file_path, 'w') as f:
+            json.dump(data, f, indent=2)
+        logger.info(f"Saved file: {file_path}")
     
 
 def export_orgunits():
@@ -34,8 +35,17 @@ def export_orgunits():
 
 def export_data_sets():
     data_sets = {d['id']: d['name'] for d in api.get('dataSets', params={'fields': 'id,name'}).json().get('dataSets')}
+    logger.info(f"Found {len(data_sets)} datasets")
     for uid, name in data_sets.items():
-        logger.info("Found data sets:")
+        logger.info(f" - {name} ({uid})")
+        data = api.get(f'dataSets/{uid}/metadata').json()
+        slug_name = f"2_dataset_{slugify(name)}_{uid}"
+        save_json(data, slug_name)
+
+def export_dashboards():
+    dashboards = {d['id']: d['name'] for d in api.get('dashboards', params={'fields': 'id,name'}).json().get('dashboards')}
+    logger.info(f"Found {len(dashboards)} dashboards")
+    for uid, name in dashboards.items():
         logger.info(f" - {name} ({uid})")
         data = api.get(f'dataSets/{uid}/metadata').json()
         slug_name = f"2_dataset_{slugify(name)}_{uid}"
@@ -46,6 +56,7 @@ def main():
     logger.info(f"Starting export of configuration from {api.base_url}")
     export_orgunits()
     export_data_sets()
+    export_dashboards()
 
 if __name__ == '__main__':
     main()
